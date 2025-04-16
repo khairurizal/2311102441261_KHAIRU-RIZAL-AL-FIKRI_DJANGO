e1fe7ac07e3e2a163e575adf3b35f73917828260
khairurizal <khairurizalaf@gmail.com>
2025-04-16 12:49:19 +0800

    Django Templates

--- webrzl/urls.py
+++ webrzl/urls.py
@@ -3,15 +3,14 @@ from django.urls import path
 
 ## ini untuk menampilkan gambar yang sdh di upload 
 from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path, include
+
 
 from webrzl.views import home, about
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
     path('about', about, name="about"),
+    path('dashboard/', include('berita.urls'))
 ]
 
-## ini untuk menampilkan gambar yang sdh di upload 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- webrzl/views.py
+++ webrzl/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 
-
 def home(request):
-    template_name = 'home.html'
+    template_name = 'halaman/index.html'
     context = {
-        'title': 'portofolioku',
-        'description': 'portfolio saya',
+        'title': '-HOMEPAGE-',
+        'description': 'web portfolio saya',
         'body': 'Halaman home'
     }
     return render(request, template_name, context)
@@ -13,8 +12,17 @@ def home(request):
 def about(request):
     template_name = 'about.html'
     context = {
-        'title': 'selamat datang di halaman about',
+        'title': '-ABOUT PAGE-',
         'description': 'web portfolio saya',
         'body': 'Halaman about'
+    }
+    return render(request, template_name, context)
+
+def contact(request):
+    template_name = 'contact.html'
+    context = {
+        'title': '- CONTACT -',
+        'description': 'Contact',
+        'body': 'Contact Page'
     }
     return render(request, template_name, context)
